9348fe5ce0dac636c8dd109360dd26e2d0bf4b33
Trần Đăng Quang <144876181+DarkCody@users.noreply.github.com>
2024-08-01 06:18:09 +0700

    Update tempCodeRunnerFile.cpp

--- temp/tempCodeRunnerFile.cpp
+++ temp/tempCodeRunnerFile.cpp
@@ -1,47 +1,34 @@
+/**
+  author : cody
+**/
+
+
+/*
+    /\_/\
+   (= ._.)
+  / >0   \>1
+*/
+
 #include <bits/stdc++.h>
+#define taskname ""
+#define ull unsigned long long
+#define ll long long
+#define endl "\n"
 
 using namespace std;
+const int nmax = 1e6, INF = 1e9 + 7;
 
-bool check (long long x)
-{
-    for (long long i=2;i<=sqrt(x);++i)
-    if (x%i==0)return false;
-    return x>1;
-}
-long long tcs(long long x)
-{
-    long long s=0;
-    while(x!=0)
-    {
-        s+=(x%10)*(x%10);
-        x/=10;
-    }
-    return s;
-}
-bool check1(long long n)
-{
-    long long s=0;
-    while(n>=0)
-    {
-        n=tcs(n);
-    }
-    return n==1;
-}
-long long n;
-int main()
-{
-    cin >> n;
-    for (long long i=n;i>=1;--i)
-    {
-        if (check(i) or n%i==0)
-        {
-            cout<<i<<" ";
-        }
-    }
-    cout << '\n';
-    for (long long i=n;i>=1;--i)
-    {
-        if (check1(i)) cout << i << " ";
-    }
-    return 0;
+
+int main() {
+  //freopen (taskname".inp", "r", stdin);
+  //freopen (taskname".out", "w", stdout);
+  
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+
+  
+  clock_t tStart = clock();
+  cerr<<fixed<<setprecision(10)<<"\nTime Taken: "<<(double)(clock()- tStart)/CLOCKS_PER_SEC<<endl;
+  return 0;
 }
